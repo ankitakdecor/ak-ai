1ef28db90e4da85d2861edc1180189606484cb3e
Ankitakdecor <ayush@gmail.com>
2026-08-08 16:09:47 +0530

    Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+const response = await fetch("http://127.0.0.1:8000/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
